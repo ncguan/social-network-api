d37f91d77b04a75934b65e6d96a328057ffefdd4
NG <nicholeguan2@gmail.com>
2022-11-23 12:49:56 -0800

    add route and controller to get a single user and post a new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,33 @@
 const { ObjectId } = require('mongoose').Types;
-const { User, Thought} = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     getAllUsers(req, res) {
         User.find()
-        .then(async (users) => {
-            const userObj = {
-                users
-            };
-            return res.json(userObj);
-        })
-        .catch((err) => {
-            console.log(err);
-            return res.status(500).json(err);
-        });
-    }
+            .then(async (users) => {
+                const userObj = {
+                    users
+                };
+                return res.json(userObj);
+            })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    createUser(req, res) {
+        User.create(req.body)
+            .then((userData) => res.json(userData))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((user) =>
+                !user
+                    ? res.status(404).json({ message: 'No user with that ID' })
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const{
-    getAllUsers
+    getAllUsers,
+    getSingleUser,
+    createUser
 } = require('../../controllers/userController');
 
-router.route('/').get(getAllUsers)
-//.post(createUser);
+router.route('/').get(getAllUsers).post(createUser);
 
-//router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser)//.put(updateUser).delete(deleteUser);
 
 //router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
